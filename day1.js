996e8b5028d7dcf00c8ba9656b2e65007dcdf511
Vishal-Deepak-Naik <vishalnaik7666@gmail.com>
2024-07-14 17:43:10 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,77 @@
+//Activity 1
+
+    //task 1
+    var number=7
+    console.log(number);
+
+    //task2
+    let str="vishal deepak naik"
+    console.log(str)
+    
+console.log('\n\n');
+//Activity 2
+
+    // constant     
+    const fixed=true;
+    console.log(fixed);
+
+console.log('\n\n');
+//Activity 3
+    // premitive 
+    const num=7
+    console.log(typeof num);
+
+    const string="working hard"
+    console.log(typeof string);
+
+    const boolean_value=false
+    console.log(typeof boolean_value);
+
+    const bigInt_value=34n
+    console.log(typeof bigInt_value);
+
+    const symbol_value=Symbol('vishal')
+    console.log(typeof symbol_value);
+
+    const undefined_value=undefined
+    // let undefines
+    console.log(typeof undefined_value);
+
+    const null_value=null
+    console.log(typeof null_value);
+
+    console.log('\n');
+    //non premitive
+    const arr=[4,5,6,7]
+    console.log(typeof arr);
+
+    const objName={
+        id:8,
+        name:"vishal naik"
+    }
+    console.log(typeof objName);
+
+
+console.log('\n\n');
+//Activity 4
+
+    let assign="vishal"
+    console.log(assign);
+
+    assign="vishal naik"
+    console.log(assign);
+
+
+console.log('\n');
+//Activity 5
+
+    const value=45
+    // value=100    //value is not change as variable is declared as constant
+
+
+    
+
+
+
+
+
